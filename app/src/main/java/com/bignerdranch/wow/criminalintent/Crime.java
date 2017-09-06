26576288e3233c4d7327d6df6463ccd2cc6498dd
DWCS2333 <1163256793@qq.com>
2017-09-06 23:58:49 +0800

    优化criminalFragment布局，加入事件和是否解决选择

--- app/src/main/java/com/bignerdranch/wow/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/wow/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.wow.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,6 +10,8 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public String getTitle() {
         return mTitle;
@@ -23,8 +26,25 @@ public class Crime {
 
     }
 
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
 
     }
 }
